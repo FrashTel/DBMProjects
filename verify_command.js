82f86bd5021734f46f7db5b6295453765a5c9fe8
Zaserr <14017533+Zacwilson90@users.noreply.github.com>
2018-09-22 23:56:03 -0500

    Create verify_command.js

---
+++ verify_command.js
@@ -0,0 +1,60 @@
+{
+  "name": "verify",
+  "permissions": "NONE",
+  "restriction": "1",
+  "_id": "nsqTd",
+  "actions": [
+    {
+      "info": "1",
+      "find": "Member",
+      "storage": "1",
+      "varName": "role",
+      "name": "Find Role"
+    },
+    {
+      "member": "1",
+      "varName2": "",
+      "role": "3",
+      "varName": "role",
+      "name": "Add Member Role"
+    },
+    {
+      "title": "",
+      "author": "Verify Complete",
+      "color": "#FFFFFF",
+      "timestamp": "true",
+      "url": "",
+      "authorIcon": "",
+      "imageUrl": "",
+      "thumbUrl": "",
+      "storage": "1",
+      "varName": "verifycomplete",
+      "name": "Create Embed Message"
+    },
+    {
+      "storage": "1",
+      "varName": "verifycomplete",
+      "message": "You are now been verified! You now have access to all of our channels.\n\nThanks!",
+      "name": "Set Embed Description"
+    },
+    {
+      "storage": "1",
+      "varName": "verifycomplete",
+      "channel": "0",
+      "varName2": "",
+      "name": "Send Embed Message"
+    },
+    {
+      "storage": "1",
+      "varName": "verifycomplete",
+      "channel": "1",
+      "varName2": "",
+      "name": "Send Embed Message"
+    },
+    {
+      "storage": "0",
+      "varName": "",
+      "name": "Delete Message"
+    }
+  ]
+}
