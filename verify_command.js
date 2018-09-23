4683ca6c1e12acf2776750fa5c55a9d7fc7444c9
Zaserr <14017533+Zacwilson90@users.noreply.github.com>
2018-09-23 01:29:33 -0500

    Update verify_command.js

--- verify_command.js
+++ verify_command.js
@@ -12,6 +12,77 @@
       "name": "Find Role"
     },
     {
+      "info": "0",
+      "infoIndex": "1",
+      "storage": "1",
+      "varName": "usercode",
+      "name": "Store Command Params"
+    },
+    {
+      "storage": "1",
+      "varName": "usercode",
+      "comparison": "5",
+      "value": "serverVars(\"vcode\")",
+      "iftrue": "2",
+      "iftrueVal": "12",
+      "iffalse": "0",
+      "iffalseVal": "",
+      "name": "Check Variable"
+    },
+    {
+      "title": "Verify Code Incorrect",
+      "author": "",
+      "color": "#FFFFFF",
+      "timestamp": "true",
+      "url": "",
+      "authorIcon": "",
+      "imageUrl": "",
+      "thumbUrl": "",
+      "storage": "1",
+      "varName": "wrongcode",
+      "name": "Create Embed Message"
+    },
+    {
+      "storage": "1",
+      "varName": "wrongcode",
+      "message": "${member} sorry but that is the incorrect code!",
+      "name": "Set Embed Description"
+    },
+    {
+      "storage": "1",
+      "varName": "wrongcode",
+      "channel": "0",
+      "varName2": "",
+      "varName3": "wrongcodedel",
+      "storage3": "1",
+      "name": "Send Embed Message MOD"
+    },
+    {
+      "storage": "1",
+      "varName": "wrongcode",
+      "channel": "1",
+      "varName2": "",
+      "name": "Send Embed Message"
+    },
+    {
+      "storage": "0",
+      "varName": "",
+      "name": "Delete Message"
+    },
+    {
+      "time": "10",
+      "measurement": "1",
+      "name": "Wait"
+    },
+    {
+      "storage": "1",
+      "varName": "wrongcodedel",
+      "name": "Delete Message"
+    },
+    {
+      "name": "End Action Sequence"
+    },
+    {
       "member": "1",
       "varName2": "",
       "role": "3",
@@ -19,8 +90,8 @@
       "name": "Add Member Role"
     },
     {
-      "title": "",
-      "author": "Verify Complete",
+      "title": "Verify Complete",
+      "author": "",
       "color": "#FFFFFF",
       "timestamp": "true",
       "url": "",
@@ -34,15 +105,8 @@
     {
       "storage": "1",
       "varName": "verifycomplete",
-      "message": "You are now been verified! You now have access to all of our channels.\n\nThanks!",
+      "message": "You have now been verified! You now have access to all of our channels.\n\nThanks!",
       "name": "Set Embed Description"
-    },
-    {
-      "storage": "1",
-      "varName": "verifycomplete",
-      "channel": "0",
-      "varName2": "",
-      "name": "Send Embed Message"
     },
     {
       "storage": "1",
