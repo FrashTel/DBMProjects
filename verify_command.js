e96feeb2aafe911e34d84f35641ec0df0e192b3e
Zaserr <14017533+Zacwilson90@users.noreply.github.com>
2018-09-25 17:20:00 -0500

    Update verify_command.js

--- verify_command.js
+++ verify_command.js
@@ -145,7 +145,7 @@
       "storage": "1",
       "varName": "rulesembed",
       "fieldName": "Rules",
-      "message": "Insert Rules Here",
+      "message": "Rules\n\n:red_circle: No racism, homophobia, threats, or suicide jokes at anytime\n\n:red_circle: No spamming at anytime\n\n:red_circle: Do NOT use curse words excessively \n\n:red_circle: Advertising your discord or anything else in any way shape or form is forbidden unless an advertisement purchase is made.\n\n:red_circle: Do not harass or spam any staff members in direct messages\n\n:red_circle: No impersonation of any kind is allowed (It will result in an instant permanent ban)\n\n:red_circle: Do not use bot commands in any other channel then in #bot\n\n:red_circle: No NSFW posts are allowed in any channel\n\n:red_circle: Do not abuse any of the bots in this Discord (It will result in an instant permanent ban)\n\n:red_circle: Pointless tickets using @Tickety#3703 are not allowed \n\n:red_circle: Do NOT disrespect (trash talk) this server or any staff",
       "inline": "0",
       "name": "Add Embed Field"
     },
