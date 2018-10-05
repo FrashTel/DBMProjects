3df8160214405cb34c34e8ba008be7d6ba2e441a
Zaserr <14017533+Zacwilson90@users.noreply.github.com>
2018-10-04 22:56:53 -0500

    Create Bot_Info_Interval_Event.js

---
+++ Bot_Info_Interval_Event.js
@@ -0,0 +1,18 @@
+{
+  "name": "botinfointerval",
+  "temp": "1800",
+  "event-type": "3",
+  "_id": "vcRea",
+  "actions": [
+    {
+      "storage": "2",
+      "varName": "botinfomsg1",
+      "name": "Delete Message"
+    },
+    {
+      "source": "TIVBz",
+      "type": "true",
+      "name": "Call Command/Event"
+    }
+  ]
+}
