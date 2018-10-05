7503ba6e4cd40dbafc77bddffbe326c9c5501955
Zaserr <14017533+Zacwilson90@users.noreply.github.com>
2018-10-05 15:00:03 -0500

    Create BotInfo_Setup_Command.js

---
+++ BotInfo_Setup_Command.js
@@ -0,0 +1,69 @@
+{
+  "name": "bisetup",
+  "permissions": "NONE",
+  "restriction": "2",
+  "_id": "nVBMn",
+  "actions": [
+    {
+      "info": "0",
+      "infoIndex": "1",
+      "storage": "2",
+      "varName": "botname",
+      "name": "Store Command Params"
+    },
+    {
+      "info": "0",
+      "infoIndex": "2",
+      "storage": "2",
+      "varName": "botversion",
+      "name": "Store Command Params"
+    },
+    {
+      "info": "0",
+      "infoIndex": "3",
+      "storage": "2",
+      "varName": "botowner",
+      "name": "Store Command Params"
+    },
+    {
+      "info": "0",
+      "infoIndex": "4",
+      "storage": "2",
+      "varName": "botsupport",
+      "name": "Store Command Params"
+    },
+    {
+      "info": "4",
+      "infoIndex": "1",
+      "storage": "2",
+      "varName": "bichannel",
+      "name": "Store Command Params"
+    },
+    {
+      "info": "1",
+      "infoIndex": "6",
+      "storage": "2",
+      "varName": "botdescrip",
+      "name": "Store Command Params"
+    },
+    {
+      "title": "Bot Setup Completed",
+      "author": "",
+      "color": "#ffffff",
+      "timestamp": "true",
+      "url": "",
+      "authorIcon": "",
+      "imageUrl": "",
+      "thumbUrl": "",
+      "storage": "1",
+      "varName": "botsetup",
+      "name": "Create Embed Message"
+    },
+    {
+      "storage": "1",
+      "varName": "botsetup",
+      "message": "The Bot Information System has been setup!\n\nBot Name: ${serverVars(\"botname\")}\n\nBot Description: ${serverVars(\"botdescrip\")}\n\nBot Version: ${serverVars(\"botversion\")}\n\nBot Owner: ${serverVars(\"botowner\")}\n\nBot Support Link: ${serverVars(\"botsupport\")}\n\nBot Embed Channel: ${serverVars(\"bichannel\")}",
+      "name": "Set Embed Description"
+    }
+  ]
+}
